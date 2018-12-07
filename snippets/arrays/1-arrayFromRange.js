1fc9919a3fe27c842ea95ae9218dde0cbe959177
robert win <robertchristophermorel@gmail.com>
2018-12-07 05:39:08 +0000

    Code confusing, improved documentation

--- snippets/arrays/1-arrayFromRange.js
+++ snippets/arrays/1-arrayFromRange.js
@@ -1,10 +1,15 @@
 //Create an array from a range
+//if second param is less than first return empty array
 const numbers = arrayFromRange(1, -4);
-
-console.log(numbers);
+console.log(numbers); //[]
+//if second param is bigger than first return array from range
+const numbersPlus = arrayFromRange(1, 4);
+console.log(numbersPlus); //[1,2,3,4]
 
 function arrayFromRange(min, max) {
+  //create an array
   const output = [];
+  //loop through range and add each number to array
   for (let i = min; i <= max; i++) output.push(i);
   return output;
 }
